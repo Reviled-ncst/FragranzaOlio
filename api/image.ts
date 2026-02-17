ea2cb8a5b2a055464c016505a7af1bddca7d0159
Reviled-ncst <bauto.renzrussel@ncst.edu.ph>
2026-02-17 10:34:11 +0800

    Update tunnel URL to restore-kay-pacific-runs

--- api/image.ts
+++ api/image.ts
@@ -15,7 +15,7 @@ interface VercelResponse {
 }
 
 // Backend URL for images - Cloudflare tunnel to local XAMPP
-const BACKEND_URL = process.env.BACKEND_URL || 'https://turner-removing-baking-moment.trycloudflare.com/FragranzaWeb';
+const BACKEND_URL = process.env.BACKEND_URL || 'https://restore-kay-pacific-runs.trycloudflare.com/FragranzaWeb';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   // Set CORS headers
