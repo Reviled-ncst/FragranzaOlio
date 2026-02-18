427aef0e53fb2bc454afe7463211eec1cce0e41a
Reviled-ncst <bauto.renzrussel@ncst.edu.ph>
2026-02-18 09:28:28 +0800

    Auto-update tunnel URL to https://justice-had-hiring-spam.trycloudflare.com

--- api/image.ts
+++ api/image.ts
@@ -15,7 +15,7 @@ interface VercelResponse {
 }
 
 // Backend URL for images - Cloudflare tunnel to local XAMPP
-const BACKEND_URL = process.env.BACKEND_URL || 'https://warcraft-acquisitions-kevin-elder.trycloudflare.com/FragranzaWeb';
+const BACKEND_URL = process.env.BACKEND_URL || 'https://justice-had-hiring-spam.trycloudflare.com/FragranzaWeb';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   // Set CORS headers
--- api/proxy.ts
+++ api/proxy.ts
@@ -15,7 +15,7 @@ interface VercelResponse {
 }
 
 // Backend URL - Use environment variable or default to Cloudflare tunnel
-const BACKEND_URL = process.env.BACKEND_URL || 'https://warcraft-acquisitions-kevin-elder.trycloudflare.com/FragranzaWeb';
+const BACKEND_URL = process.env.BACKEND_URL || 'https://justice-had-hiring-spam.trycloudflare.com/FragranzaWeb';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   // Set CORS headers for all responses
